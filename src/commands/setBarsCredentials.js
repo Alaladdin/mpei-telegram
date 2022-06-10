1b03f8700bec8bd66ab9bff9af2f2e2e5c898c80
Aladdin <g.tabroid@gmail.com>
2022-06-11 01:04:37 +0300

    v 1.10.1

--- src/commands/setBarsCredentials.js
+++ src/commands/setBarsCredentials.js
@@ -1,5 +1,10 @@
 export default {
   name       : 'set_bars_credentials',
   description: 'Установка данных барса',
-  execute    : (ctx) => ctx.scene.enter('SET_BARS_CREDENTIALS'),
+  execute    : (ctx) => {
+    if (ctx.isPrivateChat)
+      return ctx.scene.enter('SET_BARS_CREDENTIALS');
+
+    return ctx.replyWithMarkdown('`Команда работает только в личных сообщениях`');
+  },
 };
