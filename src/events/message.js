ba728a60fe2a7b461f125a1cda3f928b9f795a03
Tabriz Gurbanov <g.tabroid@gmail.com>
2024-02-22 19:45:44 +0300

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,16 +1,25 @@
-import { RESTRICTED_USERS } from '../constants';
+import {
+    RESTRICTED_USERS
+} from '../constants';
 
 export default {
-  name: 'message',
-  async execute(ctx) {
-    const userId = ctx.from.id;
-    const userMessage = ctx.message.text;
-    const isUserRestricted = RESTRICTED_USERS.includes(userId);
-    const isMessageRestricted = userMessage.includes('dzen.ru') || userMessage.includes('clck.ru');
+    name: 'message',
+    async execute(ctx) {
+        const userId = ctx.from.id;
+        const userMessage = ctx.message.text;
+        const isUserRestricted = RESTRICTED_USERS.includes(userId);
 
-    if (isUserRestricted && isMessageRestricted) {
-      ctx.deleteMessage(ctx.message.message_id);
-      ctx.replyWithMarkdown('`Я запрещаю вам отправлять вам видосики из Здзена ✋`');
+        if (isUserRestricted) {
+            const isMessageWithDzenLink = userMessage.includes('dzen.ru') || userMessage.includes('clck.ru');
+            const isMessageWithYouTubeLink = userMessage.includes('youtube.');
+
+            if (isMessageWithDzenLink) {
+                ctx.deleteMessage(ctx.message.message_id);
+                ctx.replyWithMarkdown('`Я запрещаю вам отправлять вам видосики из Дзена ✋`');
+            } else if (isMessageWithYouTubeLink) {
+                ctx.deleteMessage(ctx.message.message_id);
+                ctx.replyWithMarkdown('`Я запрещаю вам отправлять вам видосики из Ютуба ✋`');
+            }
+        },
     }
-  },
 };
